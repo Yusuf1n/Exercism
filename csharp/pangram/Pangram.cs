6ddb340fa8693e497860f879f075a05c5ad4fde1
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-02-10 10:53:09 +0000

    Added 4 Solutions

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        char ch = Convert.ToChar(charArray);
+
+        if (input.Contains(ch))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -0,0 +1,28 @@
+using System;
+
+public static class RnaTranscription
+{
+    public static string ToRna(string nucleotide)
+    {
+        switch (nucleotide)
+        {
+            case "G":
+                return "C";
+                break;
+
+            case "C":
+                return "G";
+                break;
+
+            case "T":
+                return "A";
+                break;
+
+            case "A":
+                return "U";
+                break;
+        }
+
+        return null;
+    }
+}
