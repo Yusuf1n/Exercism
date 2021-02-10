bcbc3002a651e1624e980ed387cbeec0075c4ee8
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-02-10 14:09:07 +0000

    Solution to 'Resistor Color Duo' Exercise

---
+++ csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -0,0 +1,22 @@
+using System;
+
+public static class ResistorColorDuo
+{
+    public static int Value(string[] colors)
+    {
+        string[] colours = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+        int input1 = int.Parse(Console.ReadLine());
+        int input2 = int.Parse(Console.ReadLine());
+
+        if (input1 <= colours.Length && input2 <= colours.Length)
+        {
+            int result = int.Parse(input1.ToString() + input2.ToString());
+            return result;
+        }
+        else
+        {
+            return int.Parse("Invalid band values"); 
+        }
+    }
+}
